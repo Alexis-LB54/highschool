e9e9f6e8dda72b1df3fd329d53c8af75b8485ad2
Alexis-LB54 <alexis@lebail.eu>
2022-03-17 16:25:50 +0100

    jeudi aprem

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('alexis.lebail@laposte.net', 'Toto University'))
+                    ->from(new Address('sandbox@loryleticee.com', 'Toto University'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
