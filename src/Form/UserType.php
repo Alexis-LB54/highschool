e016695c3903762ee08d90dd6a2c3013ef12bf5b
Alexis-LB54 <alexis@lebail.eu>
2022-03-18 11:08:20 +0100

    coucou

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,8 +7,10 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class UserType extends AbstractType
 {
@@ -47,6 +49,7 @@ class UserType extends AbstractType
                     return [$rolesString];
                 }
             ));
+        $builder->add('attachment', FileType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
